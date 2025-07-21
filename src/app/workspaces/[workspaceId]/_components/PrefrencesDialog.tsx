aef179df815e833a6ff3f86c97177e52e074b1d1
Manish <keshraram294@gmail.com>
2025-07-21 21:00:20 +0530

    -_-

--- src/app/workspaces/[workspaceId]/_components/PrefrencesDialog.tsx
+++ src/app/workspaces/[workspaceId]/_components/PrefrencesDialog.tsx
@@ -13,7 +13,7 @@ import UseConfirm from "@/components/hooks/useConfirm";
 import { useGetWorkspaceId } from "@/features/workspace/hooks/useGetWorkspaceId";
 import { Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { FormEvent, ReactNode, useState } from "react";
+import { Dispatch, FormEvent, ReactNode, SetStateAction, useState } from "react";
 import { toast } from "sonner";
 
 interface PrefrencesDialogProps {
